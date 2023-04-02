187ec62a90eb9ad7d5a2a977c0e25d3689f1cf39
bunyaminyerles <bnymnyrls@hotmail.com>
2023-04-03 01:10:19 +0300

    it adds date and hour operations

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"awesomeProject/accessModifiers"
 	"awesomeProject/enum"
-	"bufio"
 	"fmt"
-	"os"
 	"reflect"
 	"strconv"
-	"strings"
+	"time"
 )
 
 func main() {
@@ -46,17 +44,34 @@ func main() {
 	fmt.Printf("Data types : %T \n", myString3)
 	fmt.Printf("Value of myString3 variable : %v \n", myString3)
 
-	// input operations from console
-	reader := bufio.NewReader(os.Stdin)
-	myString4, _ := reader.ReadString('\n')
-	fmt.Println("input data : ", myString4)
-	// input number value
-	myString5, _ := reader.ReadString('\n')
-	myFloat2, err := strconv.ParseFloat(strings.TrimSpace(myString5), 64)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Value of number : ", myFloat2)
-	}
+	/*
+		// input operations from console
+		reader := bufio.NewReader(os.Stdin)
+		myString4, _ := reader.ReadString('\n')
+		fmt.Println("input data : ", myString4)
+		// input number value
+		myString5, _ := reader.ReadString('\n')
+		myFloat2, err := strconv.ParseFloat(strings.TrimSpace(myString5), 64)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println("Value of number : ", myFloat2)
+		}
+	*/
 
+	// Date and hour operations
+	specificDate := time.Date(2023, time.April, 3, 0, 40, 0, 0, time.UTC)
+	fmt.Printf("Specific date : %s\n", specificDate)
+	fmt.Println("The month is ", specificDate.Month())
+	fmt.Println("The day is ", specificDate.Day())
+	fmt.Println("The weekday is ", specificDate.Weekday())
+	nextYear := specificDate.AddDate(1, 0, 0)
+	fmt.Printf("The next year same time is : %s\n", nextYear)
+	longFormat := "Monday, January 2, 2006"
+	fmt.Println("Long format : ", nextYear.Format(longFormat))
+	shortformat := "2/1/2006"
+	fmt.Println("Short format :", nextYear.Format(shortformat))
+	fmt.Println("Timestamp : ", nextYear.Unix())
+	now := time.Now()
+	fmt.Printf("The time now is : %s\n", now)
 }
